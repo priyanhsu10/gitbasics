8a1b16a81f2479fe5a306b5528e3434bf8d9eb39
priyanshu <priyanshuparate@gmail.com>
2021-09-04 12:56:27 +0530

    added devide function

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 console.log("this is first git pactice package");
 
-const add=(a,b)=>a+b;
- console.log(add(1,2));
- const subjtract =(a,b)=>a-b;
- console.log(subjtract(10,5));
- const multiply=(a,b)=>a*b;
- console.log(multiply(10,30))
+const add = (a, b) => a + b;
+console.log(add(1, 2));
+const subjtract = (a, b) => a - b;
+console.log(subjtract(10, 5));
+const multiply = (a, b) => a * b;
+console.log(multiply(10, 30))
+const devide = (a, b) => {
+    if (b === 0) {
+        console.log("divident cannot be zero");
+        return;
+
+    }
+    return a/b;
+
+}
+console.log(devide(1,0))
